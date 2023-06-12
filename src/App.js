4a102957be7d95f75f3e4956a3406e977d1d983e
name <moonjonghoo@gmail.com>
2023-06-12 17:08:02 +0900

    최종

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [todate, setTodate] = useState([]);
+  const [todolist, setTodolist] = useState([]);
+  const submitHandler = () => {
+    setTodolist((prevestate) => [
+      ...prevestate,
+      { id: Math.random(), Todo: todo, date: todate },
+    ]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          테스트 수정사항입니다.
-        </a>
-      </header>
+      <h2>Dummy Todo</h2>
+      <form onSubmit={submitHandler}>
+        <p>할일</p>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        ></input>
+        <p>날짜</p>
+        <input
+          type="date"
+          value={todate}
+          onChange={(e) => setTodate(e.target.value)}
+        ></input>
+        <button type="submit">등록</button>
+      </form>
+      {console.log(todolist.Todo)}
     </div>
   );
 }
